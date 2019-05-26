0a99636ed25cbe51b7453b44ad7b0bb14a5d9743
Alejandro Jarabo <a.jarabo@alumnos.upm.es>
2019-05-26 18:01:54 +0200

    Tips bien actualizados y mejora vistas

---
+++ controllers/stats.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+const {models} = require("../models");
+const Op = Sequelize.Op;
+const paginate = require('../helpers/paginate').paginate;
+var ssn;
+
+// GET /stats
+exports.stats = (req,res,next) => {
+
+}
+
+exports.userstats = (req,res,next) => {
+	
+}
